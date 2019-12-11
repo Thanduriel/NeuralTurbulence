97db4d3cb3fedc26b9a230e3d67f399e8ff6dcfe
Thanduriel <rojendersie@alice.de>
2019-12-11 19:59:39 +0100

    + runmodel2 can make video of low freq input

--- timeseries/runmodel2.py
+++ timeseries/runmodel2.py
@@ -14,10 +14,12 @@ parser.add_argument('--output', default='predicted.mp4')
 parser.add_argument('model', default='model3.h5')
 parser.add_argument('--begin', type=int, default=0)
 parser.add_argument('--reference', dest='showReference', action='store_true')
+parser.add_argument('--lowfreq', dest='showLowFreq', action='store_true')
 parser.add_argument('--no-predict', dest='predict', action='store_false')
 parser.add_argument('--compute-error', dest='computeError', action='store_true')
 parser.set_defaults(fullpredict=False)
 parser.set_defaults(showReference=False)
+parser.set_defaults(showLowFreq=False)
 parser.set_defaults(predict=True)
 parser.set_defaults(computeError=False)
 
@@ -58,11 +60,23 @@ if args.showReference:
 		vorticity = frequency.invTransform(frequency.flattenComplex(outputFrames[i]))
 		arrayToImgFile(vorticity.real, "temp/original_{0}.png".format(i))
 
+if args.showLowFreq: 
+	highRes = np.zeros(outputFrames[0].shape)
+	lowResSize = np.array(inputs[0].shape)
+	begin = ((np.array(highRes.shape) - lowResSize) / 2).astype(int)
+	end = (begin + lowResSize).astype(int)
+	for i in range(len(inputs)):
+		highRes[begin[0]:end[0],begin[1]:end[1]] = inputs[i]
+		vorticity = frequency.invTransform(frequency.flattenComplex(highRes))
+		arrayToImgFile(vorticity.real, "temp/lowfreq_{0}.png".format(i))
+
 print("Creating video.")
 if args.showReference:
 	subprocess.run("ffmpeg -framerate 24 -i temp/original_%0d.png -vf scale=64:64 original{}.mp4".format(args.model))
 if predict:
 	subprocess.run("ffmpeg -framerate 24 -i temp/predicted_%0d.png -vf scale=64:64 predicted{}.mp4".format(args.model))
+if args.showLowFreq:
+	subprocess.run("ffmpeg -framerate 24 -i temp/lowfreq_%0d.png -vf scale=64:64 lowfreq{}.mp4".format(args.model))
 
 def error(array1, array2):
 	dif = np.subtract(array1, array2)
